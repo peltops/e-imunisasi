9e5f83a5d383eb7a91021ff7d38e87da0b84ab0c
rfaturriza <56538058+rfaturriza@users.noreply.github.com>
2020-11-28 14:03:29 +0700

    update add_event kalender

--- e_imunisasi/lib/pages/kalenderpage/add_event.dart
+++ e_imunisasi/lib/pages/kalenderpage/add_event.dart
@@ -1,4 +1,5 @@
 import 'package:e_imunisasi/models/user.dart';
+import 'package:e_imunisasi/pages/profilepage/widget/textdecoration.dart';
 import 'package:e_imunisasi/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,7 +10,6 @@ class AddEventPage extends StatefulWidget {
 }
 
 class _AddEventPageState extends State<AddEventPage> {
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   TextEditingController _title;
   TextEditingController _description;
   DateTime _eventDate;
@@ -31,7 +31,14 @@ class _AddEventPageState extends State<AddEventPage> {
     final user = Provider.of<User>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add note"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0.0,
+        title: Text(
+          "Tambah event kalender",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       key: _key,
       body: Form(
@@ -47,13 +54,10 @@ class _AddEventPageState extends State<AddEventPage> {
                   controller: _title,
                   validator: (value) =>
                       (value.isEmpty) ? "Please Enter title" : null,
-                  style: style,
-                  decoration: InputDecoration(
-                      labelText: "Title",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10))),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  decoration: textDecoration.copyWith(
+                    labelText: 'Title',
+                  ),
                 ),
               ),
               Padding(
@@ -65,11 +69,10 @@ class _AddEventPageState extends State<AddEventPage> {
                   maxLines: 5,
                   validator: (value) =>
                       (value.isEmpty) ? "Please Enter description" : null,
-                  style: style,
-                  decoration: InputDecoration(
-                      labelText: "description",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10))),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  decoration: textDecoration.copyWith(
+                    labelText: 'Description',
+                  ),
                 ),
               ),
               const SizedBox(height: 10.0),
@@ -94,39 +97,97 @@ class _AddEventPageState extends State<AddEventPage> {
               processing
                   ? Center(child: CircularProgressIndicator())
                   : Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Theme.of(context).primaryColor,
-                        child: MaterialButton(
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              setState(() {
-                                processing = true;
-                              });
-                              final DataKalender product = DataKalender(
-                                  id: user.uid,
-                                  title: _title.text,
-                                  description: _description.text,
-                                  eventDate: _eventDate);
-                              await DatabaseService(uid: user.uid)
-                                  .addEventKalender(product.toMap());
-                              Navigator.pop(context);
-                              setState(() {
-                                processing = false;
-                              });
-                            }
-                          },
-                          child: Text(
-                            "Save",
-                            style: style.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                      child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                    color: Colors.grey.shade200,
+                                    offset: Offset(4, 2),
+                                    blurRadius: 5,
+                                    spreadRadius: 2)
+                              ],
+                              gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Color(0xfffbb448),
+                                    Color(0xfff7892b)
+                                  ])),
+                          child: FlatButton(
+                            onPressed: !processing
+                                ? () async {
+                                    if (_formKey.currentState.validate()) {
+                                      setState(() {
+                                        processing = true;
+                                      });
+                                      final DataKalender product = DataKalender(
+                                          id: user.uid,
+                                          title: _title.text,
+                                          description: _description.text,
+                                          eventDate: _eventDate);
+                                      await DatabaseService(uid: user.uid)
+                                          .addEventKalender(product.toMap());
+                                      Navigator.pop(context);
+                                      setState(() {
+                                        processing = false;
+                                      });
+                                    }
+                                  }
+                                : null,
+                            child: !processing
+                                ? Text(
+                                    "Edit",
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                : SizedBox(
+                                    height: 18,
+                                    width: 18,
+                                    child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.white),
+                                    ),
+                                  ),
+                          )),
                     ),
+              // Padding(
+              //     padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              //     child: Material(
+              //       elevation: 5.0,
+              //       borderRadius: BorderRadius.circular(30.0),
+              //       color: Theme.of(context).primaryColor,
+              //       child: MaterialButton(
+              //         onPressed: () async {
+              //           if (_formKey.currentState.validate()) {
+              //             setState(() {
+              //               processing = true;
+              //             });
+              //             final DataKalender product = DataKalender(
+              //                 id: user.uid,
+              //                 title: _title.text,
+              //                 description: _description.text,
+              //                 eventDate: _eventDate);
+              //             await DatabaseService(uid: user.uid)
+              //                 .addEventKalender(product.toMap());
+              //             Navigator.pop(context);
+              //             setState(() {
+              //               processing = false;
+              //             });
+              //           }
+              //         },
+              //         child: Text(
+              //           "Save",
+              //           style: style.copyWith(
+              //               color: Colors.white,
+              //               fontWeight: FontWeight.bold),
+              //         ),
+              //       ),
+              //     ),
+              //   ),
             ],
           ),
         ),
