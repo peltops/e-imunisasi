c417f18d74537d04f5b5d7a55588776e9340a3d3
Rizky Faturriza <rfaturriza@gmail.com>
2023-06-28 18:49:21 +0700

    Fix: FirebaseStorage Injection

--- lib/di/app_module.dart
+++ lib/di/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,6 +14,8 @@ abstract class AppModule {
   FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
   @injectable
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
+  @injectable
+  FirebaseStorage get firebaseStorage => FirebaseStorage.instance;
   @preResolve
   Future<SharedPreferences> get sharedPreferences =>
       SharedPreferences.getInstance();
