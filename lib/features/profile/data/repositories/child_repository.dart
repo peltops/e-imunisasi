9da21d1d57f45df8552d722255c35a5c344c93b5
Rizky Faturriza <rfaturriza@gmail.com>
2023-09-19 11:45:51 +0700

    refactor(childProfile): added child_repository

---
+++ lib/features/profile/data/repositories/child_repository.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:injectable/injectable.dart';
+
+import '../models/anak.dart';
+
+@injectable
+class ChildRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth firebaseAuth;
+  final FirebaseStorage firebaseStorage;
+
+  static const String _collectionName = 'children';
+
+  ChildRepository(
+    this.firestore,
+    this.firebaseAuth,
+    this.firebaseStorage,
+  );
+
+  Future<List<Anak>> getAllChildren() async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    final data = await firestore
+        .collection(_collectionName)
+        .where(
+          'parent_id',
+          isEqualTo: _currentUser.uid,
+        )
+        .get();
+    return data.docs.map((e) {
+      var data = Map<String, dynamic>.from(e.data());
+      data['id'] = e.id;
+      return Anak.fromMap(data);
+    }).toList();
+  }
+
+  Future<Anak> setChild(Anak anak) async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    try {
+      final data = anak.copyWith(parentId: _currentUser.uid);
+      final result =
+          await firestore.collection(_collectionName).add(data.toMap());
+      return data.copyWith(id: result.id);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> updateChild(Anak anak) async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    return await firestore
+        .collection(_collectionName)
+        .doc(anak.id)
+        .update(anak.toMap());
+  }
+
+  Future<void> deleteChild(String id) async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    return await firestore.collection(_collectionName).doc(id).delete();
+  }
+
+  Future<String> _uploadImage(File filePath, String id) async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    final ref = firebaseStorage.ref().child(id);
+    final result = await ref.putFile(filePath);
+    final fileUrl = await result.ref.getDownloadURL();
+    return fileUrl;
+  }
+
+  Future<void> updateChildAvatar({
+    required File file,
+    required String id,
+  }) async {
+    final _currentUser = firebaseAuth.currentUser;
+    if (_currentUser == null) {
+      throw Exception('User not logged in');
+    }
+    try {
+      final url = await _uploadImage(file, id);
+      await firestore.collection(_collectionName).doc(id).update(
+        {'photo_url': url},
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
