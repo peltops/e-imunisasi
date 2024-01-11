f3ffb570b6cf721b5542641bf2f179acc34c64da
Rizky Faturriza <rfaturriza@gmail.com>
2024-01-11 10:41:11 +0700

    refactor (calendar-repo): add model calendar activity hive

---
+++ lib/features/calendar/data/models/hive_calendar_activity_model.dart
@@ -0,0 +1,11 @@
+import 'package:hive/hive.dart';
+part 'hive_calendar_activity_model.g.dart';
+
+@HiveType(typeId: 0)
+class CalendarActivityHive extends HiveObject {
+  @HiveField(0)
+  DateTime? date;
+
+  @HiveField(1)
+  String? activity;
+}
