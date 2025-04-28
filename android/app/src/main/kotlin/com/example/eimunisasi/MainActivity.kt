e647cc63349d938ab218f18d11a89b864ad3b9e5
nafas-rizkyfaturriza <rizky.faturriza@nafas.co.id>
2025-04-28 15:34:24 +0700

    Update Android build configuration, add ProGuard rules for Stripe, and integrate flutter_stripe dependency

--- android/app/src/main/kotlin/com/example/eimunisasi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/eimunisasi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.peltops.e_imunisasi
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
